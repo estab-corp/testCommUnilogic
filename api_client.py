742b1c1ac5f38cb4750c8ba67e58f0b748666170
manuel deneu <manuel.deneu@gmail.com>
2026-04-08 12:21:03 +0200

    make sure connection is reset on closing

--- api_client.py
+++ api_client.py
@@ -20,19 +20,20 @@ class APIClient:
     def _run(self):
         self._running = True
         print("Running thread")
-        while self._running is True:
-            if self._stop_thread_req:
-                break
-            assert self.client
-            b = self.client.recv(1)
-            if len(b) == 0:
-                self.disconnect()
-                break
-            self.received_bytes += b
-
-        print("After thread")
-        self._running = False
-        self._thread = None
+        try:
+            while self._running is True:
+                if self._stop_thread_req:
+                    break
+                assert self.client
+                b = self.client.recv(1)
+                if len(b) == 0:
+                    self.disconnect()
+                    break
+                self.received_bytes += b
+        finally:
+            print("After thread")
+            self._running = False
+            self._thread = None
 
     def connected(self) -> bool:
         return self.client is not None
@@ -49,7 +50,7 @@ class APIClient:
             self._stop_thread_req = False
             self._thread.start()
         except Exception as err:
-            self.logger.print(f"conn error {type(err)} {err}")
+            self.logger.print(f"conn error 3: {type(err)} {str(err)}")
             self.client = None
 
     def disconnect(self):
@@ -65,7 +66,7 @@ class APIClient:
         try:
             self.client.send(data)
         except Exception as err:
-            self.logger.print(f"conn error {type(err)} {err}")
+            self.logger.print(f"conn error 1: {type(err)} {str(err)}")
             self.client = None
 
     def _recv(self, size):
@@ -73,7 +74,7 @@ class APIClient:
         try:
             return self.client.recv(size)
         except Exception as err:
-            self.logger.print(f"conn error {type(err)} {err}")
+            self.logger.print(f"conn error 2: {type(err)} {str(err)}")
             self.client = None
         return None
 
