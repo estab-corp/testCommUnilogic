35767e3d13484345510ee948367081e45de3152c
manuel deneu <manuel.deneu@gmail.com>
2026-04-17 09:42:45 +0200

    use one entry point for both client and server

--- api_emul/server.py
+++ api_emul/server.py
@@ -1,6 +1,17 @@
 import socket
+from api_client import MsgParser
 
-if __name__ == "__main__":
+
+def client_loop(conn: socket.socket):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            print("close connection")
+            return
+        print(f"got data {data}")
+
+
+def server_loop():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('', 9000))
     server.listen(1)
@@ -8,9 +19,4 @@ if __name__ == "__main__":
         print("Waiting for a connection")
         conn, addr = server.accept()
         print(f"Connected with {addr[0]}:{str(addr[1])}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("close connection")
-                break
-            print(f"got data {data}")
+        client_loop(conn)
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import argparse
 from main_win import MainWindow
+from api_emul.server import server_loop
+
+
+parser = argparse.ArgumentParser(prog='Test com')
+parser.add_argument("--server", action="store_true")
 
 
 def main():
@@ -7,5 +13,13 @@ def main():
     main_win.cleanup()
 
 
+def main_server():
+    server_loop()
+
+
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    if args.server:
+        main_server()
+    else:
+        main()
